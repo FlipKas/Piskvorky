3c07ff6093ac405dfc1cc69112aaaebd0e8eeabc
FlipKas <115064221+FlipKas@users.noreply.github.com>
2023-05-01 19:28:07 +0200

    Add files via upload

---
+++ Zastaveni hry po vyhre.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //private void reset()
+            //{
+            //    try
+            //    {
+            //        foreach (Control c in Controls)
+            //        {
+            //            Button b = (Button)c;
+            //            b.Enabled = false;
+            //        }
+            //    }
+            //    catch { };
+
+
+
+            //}
+        }
+    }
+}
---
+++ Zmacknuti buttonu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //private void zmacknuto(object sender, EventArgs e)
+            //{
+            //    Button b = (Button)sender;
+            //    if (poradi)
+            //    {
+            //        b.Text = "X";
+            //    }
+            //    else
+            //    {
+            //        b.Text = "O";
+            //    }
+            //    poradi = !poradi;
+
+            //    b.Enabled = false;
+            //    poradi_cislo++;
+            //    vyhraCheck();
+            //}
+
+        }
+    }
+}
